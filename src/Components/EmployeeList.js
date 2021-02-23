5b3e23809a9a6898843a43cb5f05b8cd4636b650
Pedrolustosa <pedroeternalss@gmail.com>
2021-02-23 10:51:30 -0300

    Button at cofirmation of the delete

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -40,56 +40,74 @@ export class EmployeeList extends Component {
                 <div className="d-flex justify-content-center">
                     <h1 className="text-center border-5 border-bottom border-success w-50 m-3 p-2">Bem-vindo</h1>
                 </div>
+                <div className="d-flex justify-content-center">
+                <a href="https://github.com/Pedrolustosa" class="btn btn-lg btn-outline-dark"><i
+                    class="fab fa-github"></i> GitHub</a>
+                <a href="https://www.linkedin.com/in/pedro-henrique-lustosa-e-silva-29b827144/"
+                  class="btn btn-lg btn-outline-primary"><i class="fab fa-linkedin"></i> Linkedin</a>
+                </div>
                 <hr/>
                     <EmployeeForm/>
                 <hr/>
-                <h3 className="text-center">Lista de Empregados</h3>
-                <div className="table-responsive">
-                <table className="table table-sm table-striped table-bordered table-hover table-dark text-center">
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Aniversário</th>
-                            <th>Gênero</th>
-                            <th>Email</th>
-                            <th>CPF</th>
-                            <th>Data de Início</th>
-                            <th>Equipe</th>
-                            <th>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.props.list.map((item, index) => {
-                                return <tr key={index}>
-                                    <td>{item.Name}</td>
-                                    <td>{item.BirtDate}</td>
-                                    <td>{item.Gender}</td>
-                                    <td>{item.Email}</td>
-                                    <td>{item.CPF}</td>
-                                    <td>{item.StartDate}</td>
-                                    <td>{item.Team}</td>
-                                    <td className="justify-content-center">
-                                        <div className="btn-group">
-                                            <button type="button" className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                                                Ações
-                                            </button>
-                                            <ul className="dropdown-menu">
-                                                <li><a className="dropdown-item"><button className="btn btn-sm btn-outline-warning" onClick={() => this.handleEdit(index)}>Editar</button></a></li>
-                                                <li><a className="dropdown-item"><button className="btn btn-sm btn-outline-danger" onClick={() => this.handleDelete(index)}>Excluir</button></a></li>
-                                            </ul>
-                                        </div>
-                                    </td>
+                <div className="card">
+                    <div className="card-body">
+                        <div className="card-header">
+                            <h3 className="text-center fw-bold">Lista de Empregados</h3>
+                        </div>
+                        <div className="table-responsive">
+                        <table className="table table-sm table-striped table-bordered table-hover table-dark text-center">
+                            <thead>
+                                <tr>
+                                    <th>Nome</th>
+                                    <th>Aniversário</th>
+                                    <th>Gênero</th>
+                                    <th>Email</th>
+                                    <th>CPF</th>
+                                    <th>Data de Início</th>
+                                    <th>Equipe</th>
+                                    <th>Ações</th>
                                 </tr>
-                            })
-                        }
-                    </tbody>
-                </table>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.props.list.map((item, index) => {
+                                        return <tr key={index}>
+                                            <td>{item.Name}</td>
+                                            <td>{item.BirtDate}</td>
+                                            <td>{item.Gender}</td>
+                                            <td>{item.Email}</td>
+                                            <td>{item.CPF}</td>
+                                            <td>{item.StartDate}</td>
+                                            <td>{item.Team}</td>
+                                            <td>
+                                            <div class="d-grid gap-2">
+                                                <div class="btn-group dropstart">
+                                                    <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                                        Opções
+                                                    </button>
+                                                        <ul class="dropdown-menu">
+                                                            <li><a className="dropdown-item"><button className="btn btn-outline-warning w-100" onClick={() => this.handleEdit(index)}>Editar</button></a></li>
+                                                            <li><a className="dropdown-item"><button className="btn btn-outline-danger w-100 delete-button" onClick={() => { if (window.confirm('Are you sure you wish to delete this item?')) this.handleDelete(index) }} >Excluir</button></a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                
                 </div>
             </div>
         )
     }
 }
+
+
+
 
 const mapStateToProps = state => {
     return {
